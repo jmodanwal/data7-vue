2b9bce360c3f204a179b24632e15bc81674d66d5
Jofferson R Tiquez <jofftiquez@gmail.com>
2021-05-11 22:20:01 +0800

    refactor: format document, add key binding to v-for

--- components/usp.vue
+++ components/usp.vue
@@ -1,25 +1,22 @@
 <template>
-  <section class="section bg-light pt-5">
+  <section id="counter" class="section bg-light pt-5">
     <div class="container">
       <div class="row" id="usp">
         <div class="col-lg-5">
           <div class="counter-info mt-4">
             <h3>{{title}}</h3>
-            <p class="text-muted mt-4">
-              {{description}}
-            </p>
+            <p class="text-muted mt-4">{{description}}</p>
             <div class="mt-4">
-              <a :href="cta_url" class="btn btn-primary"
-                >{{cta_title}}<i class="mdi mdi-arrow-right"></i
-              ></a>
+              <a :href="cta_url" class="btn btn-primary">
+                {{cta_title}}
+                <i class="mdi mdi-arrow-right"></i>
+              </a>
             </div>
           </div>
         </div>
         <div class="col-lg-7">
-		
           <div class="row">
-		  
-            <div class="col-lg-6" v-for="(usp, i) in usp">
+            <div class="col-lg-6" v-for="(usp, i) in usp" :key="i">
               <div class="counter-box mt-4">
                 <div class="media box-shadow bg-white p-4 rounded">
                   <div class="counter-icon mr-3">
@@ -27,17 +24,12 @@
                   </div>
                   <div class="media-body pl-2">
                     <h5 class="mt-2 mb-0 f-17">{{usp.title}}</h5>
-                    <p class="text-muted mb-0 mt-2 f-15">
-                      {{usp.description}}
-                    </p>
+                    <p class="text-muted mb-0 mt-2 f-15">{{usp.description}}</p>
                   </div>
                 </div>
               </div>
             </div>
-
-            
           </div>
-		  
         </div>
       </div>
     </div>
